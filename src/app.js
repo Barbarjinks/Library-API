dc62f81b083d93d699edbdf142e3e30f3beab6ad
Cai Jenkins <Caisjenkins@gmail.com>
2019-09-29 14:02:00 +0100

    added and complete add user test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const userControllers = require('./controllers/users');
 
 const app = express();
 app.use(express.json());
+
+app.post('/users', userControllers.create);
 
 module.exports = app;
